0bbedc53b1c1fe7ef15bc3e7eb1fdf342615c9b0
rpsohag <rpsohag.bd@gmail.com>
2025-02-01 11:31:36 +0600

    making url pretty and add htaccess file for handle request

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
+$segments = explode('/', $path);
 
-
-
-$action = $_GET['action'];
-$controller = $_GET['controller'];
+$action = $segments[2];
+$controller = $segments[1];
 
 require "src/controllers/$controller.php";
 
